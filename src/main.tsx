044a790a40c722c8dfa68916f3036529f576820a
Alessandro Scandone <alescandone@gmail.com>
2022-11-10 22:51:48 +0100

    fix base url

--- src/main.tsx
+++ src/main.tsx
@@ -9,7 +9,19 @@ import { AuthProvider } from "src/providers/Auth";
 
 import "./index.css";
 
-const BASE_URL = "http://localhost:3000";
+function getBaseUrl() {
+  if (typeof window !== "undefined") {
+    // browser should use relative path
+    return "";
+  }
+
+  if (process.env.VERCEL_URL) {
+    return `https://${process.env.VERCEL_URL}`;
+  }
+
+  // assume localhost
+  return `http://localhost:${process.env.PORT ?? 3000}`;
+}
 
 const App = () => {
   const [queryClient] = useState(() => new QueryClient());
@@ -17,7 +29,7 @@ const App = () => {
     trpc.createClient({
       links: [
         httpBatchLink({
-          url: `${BASE_URL}/api/trpc`,
+          url: `${getBaseUrl()}/api/trpc`,
         }),
       ],
     })
