40ad69ac62efc5a32f063060cc6c93d2850132be
Alessandro Scandone <alescandone@gmail.com>
2022-11-10 23:53:14 +0100

    edit form padding

--- src/components/CenterForm.tsx
+++ src/components/CenterForm.tsx
@@ -4,7 +4,7 @@ export const CenterForm: FC<{
   header: string;
   children: ReactNode;
 }> = ({ header, children }) => (
-  <div className="mx-auto max-w-sm my-10">
+  <div className="mx-auto max-w-sm my-10 px-4 md:px-0">
     <h4 className="text-xl font-semibold text-center">{header}</h4>
     <div className="h-8"></div>
     {children}
