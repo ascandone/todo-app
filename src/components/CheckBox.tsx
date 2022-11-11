4a43f69ed61352d51c4be2e60c9f67d9779716f7
Alessandro Scandone <alescandone@gmail.com>
2022-11-11 01:20:16 +0100

    tweak ui

--- src/components/CheckBox.tsx
+++ src/components/CheckBox.tsx
@@ -19,10 +19,11 @@ export const CheckBox: FC<{
       type="button"
       onClick={() => onToggle(!value)}
       className={classNames(
-        "cursor-pointer border border-slate-300 rounded-md shadow-sm w-5 h-5 flex items-center justify-center hover:ring-2 ring-slate-300 ",
+        "cursor-pointer border border-slate-300 rounded-md shadow-sm w-5 h-5 flex items-center justify-center ring-slate-300 ",
         "transition-colors duration-150 ease-in-out",
         {
-          "bg-slate-800 border-slate-800": value,
+          "hover:bg-slate-300": !value,
+          "bg-slate-800 border-slate-800 hover:bg-slate-700": value,
         }
       )}
     >
