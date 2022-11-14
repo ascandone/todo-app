86b3bb415bf238bff628067fba87963687ee0ee4
Alessandro Scandone <alescandone@gmail.com>
2022-11-14 21:37:24 +0100

    edit alert style

--- src/components/Alert.tsx
+++ src/components/Alert.tsx
@@ -11,15 +11,18 @@ export const Alert: FC<AlertProps> = ({ children, type = "default" }) => (
   <div
     className={classNames(
       "px-4 py-4 rounded max-w-md text-lg",
-      "flex space-x-4 items-center",
+      "flex space-x-4 items-center border",
       {
-        "bg-red-50 text-red-900": type === "error",
+        "bg-sky-50 text-sky-900 border-sky-300": type === "default",
+        "bg-red-50 text-red-900  border-red-300": type === "error",
       }
     )}
   >
-    <span>
-      <XCircleIcon className="h-6" />
-    </span>
+    {type === "error" ? (
+      <span>
+        <XCircleIcon className="h-6" />
+      </span>
+    ) : null}
     <span>{children}</span>
   </div>
 );
