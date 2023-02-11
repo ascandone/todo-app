b550c62c54759683c0dc5ee57d0b981a89dde846
Alessandro Scandone <alescandone@gmail.com>
2023-02-11 11:13:37 +0100

    revert

--- api/trpc/[trpc].ts
+++ api/trpc/[trpc].ts
@@ -1,6 +1,5 @@
 import { createNextApiHandler } from "@trpc/server/adapters/next";
-// eslint-disable-next-line @typescript-eslint/no-var-requires
-const { appRouter } = require("../../src/backend/router");
+import { appRouter } from "../../src/backend/router";
 
 export default createNextApiHandler({
   router: appRouter,
