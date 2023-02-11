ba1c0145ccb461562471301a286ea1e26b0af045
Alessandro Scandone <alescandone@gmail.com>
2023-02-11 12:09:17 +0100

    added remove err btn

--- src/components/CenterForm.tsx
+++ src/components/CenterForm.tsx
@@ -1,4 +1,4 @@
-import { FC, ReactNode } from "react";
+import { FC, ReactNode, useEffect, useState } from "react";
 import { Alert } from "src/components/Alert";
 
 export const CenterForm: FC<{
@@ -6,18 +6,30 @@ export const CenterForm: FC<{
   children: ReactNode;
   bottom: ReactNode;
   error?: string;
-}> = ({ header, children, bottom, error }) => (
-  <div className="mx-auto max-w-sm my-24 px-4 md:px-0">
-    <h4 className="text-xl font-semibold text-center">{header}</h4>
-    <div className="h-8"></div>
-    {error === undefined ? null : (
-      <div className="mb-10">
-        <Alert type="error">{error}</Alert>
-      </div>
-    )}
+}> = ({ header, children, bottom, error }) => {
+  const [showError, setShowError] = useState(error !== undefined);
 
-    {children}
-    <div className="h-2"></div>
-    <p className="mt-7 text-center text-slate-700 text-sm">{bottom}</p>
-  </div>
-);
+  useEffect(() => {
+    if (error !== undefined) {
+      setShowError(true);
+    }
+  }, [error]);
+
+  return (
+    <div className="mx-auto max-w-sm my-24 px-4 md:px-0">
+      <h4 className="text-xl font-semibold text-center">{header}</h4>
+      <div className="h-8"></div>
+      {error === undefined || !showError ? null : (
+        <div className="mb-10">
+          <Alert type="error" onDelete={() => setShowError(false)}>
+            {error}
+          </Alert>
+        </div>
+      )}
+
+      {children}
+      <div className="h-2"></div>
+      <p className="mt-7 text-center text-slate-700 text-sm">{bottom}</p>
+    </div>
+  );
+};
